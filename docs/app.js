78cfc9ee3a1013939192b4c0c89a6ac58de515f9
Jeremy Osborne <jeremywosborne@gmail.com>
2019-12-03 11:05:28 -0800

    clean up

--- docs/app.js
+++ docs/app.js
@@ -1,30 +1,74 @@
 /* global interact */
 (function () {
   // Gridiron internals as old fashioned invoke immediate function.
+  const gridiron = {};
 
-  // Async load an image from URI `src` and invokes callback with signature
-  // callback(error, ImgElement) when done.
-  const imagePreloader = function (src, callback) {
-    const preloader = document.createElement('img')
-    preloader.src = src
-    preloader.style.left = '-9000px'
-    preloader.style.position = 'fixed'
-    preloader.style.top = '-9000px'
-    // Good enough job of determining dimensions.
-    preloader.onload = function () {
-      callback(null, this)
+  (function (exports) {
+    exports.image = {}
+    // Async load an image from URI `src` and invokes callback with signature
+    // `callback(error, ImgElement)` when done.
+    const preload = function (src, callback) {
+      const preloader = document.createElement('img')
+      preloader.src = src
+      preloader.style.left = '-9000px'
+      preloader.style.position = 'fixed'
+      preloader.style.top = '-9000px'
+      // Good enough job of determining dimensions.
+      preloader.onload = function () {
+        callback(null, this)
+      }
     }
-  };
+    // no need to make this public anymore.
+    // exports.image.preload = preload
+
+    // Async load an image from URI `src` and invokes callback when done
+    // with signature `callback(error)`.
+    // Handles loading message in UI.
+    const load = function (src, callback) {
+      const loadingMessage = document.querySelector('#gridirion-map-image-loading')
+      const img = document.querySelector('#gridiron-map-image')
+
+      // Loading in process.
+      loadingMessage.style.display = 'block'
+      img.style.display = 'none'
+
+      preload(src, function (error, preloaded) {
+        if (error) {
+          // Shouldn't actually get here.
+          throw error
+        }
+        img.src = src
+        img.style.maxHeight = preloaded.height + 'px'
+        img.style.maxWidth = preloaded.width + 'px'
+
+        // Loading done.
+        loadingMessage.style.display = 'none'
+        img.style.display = 'block'
+        callback(null)
+      })
+    }
+    exports.image.load = load
+
+    // What are the dimensions of the loaded map image?
+    const dimensions = function () {
+      const el = document.querySelector('#gridiron-map')
+      return {height: el.offsetHeight, width: el.offsetWidth}
+    }
+    exports.image.dimensions = dimensions
+  })(gridiron);
 
   // Grid controllers for the prototype.
-  (function () {
+  (function (exports) {
     // Pixels between gridlines...
     const MIN_DEFAULT_DISTANCE = 100
 
-    // Get dimensions of the container. For prototype, ignore browser resize and zoom.
-    const mapDimensions = function () {
-      const el = document.querySelector('#gridiron-map')
-      return {height: el.offsetHeight, width: el.offsetWidth}
+    // Clears any existing gridlines.
+    const clear = function () {
+      const nodes = document.querySelectorAll('[class*="gridiron-gridline"]')
+      for (let i = 0; i < nodes.length; i++) {
+        const node = nodes[i]
+        node.parentNode.removeChild(node)
+      }
     }
 
     // axis is `x` or `y`
@@ -55,7 +99,7 @@
 
     // axis is `x` or `y`
     const renderGridlines = function (axis) {
-      const dims = mapDimensions()
+      const dims = gridiron.image.dimensions()
       const offset = axis === 'x' ? dims.width : dims.height
       const gridlineLength = axis === 'x' ? dims.height : dims.width
       const container = document.querySelector('#gridiron-map-gridlines-' + axis)
@@ -74,15 +118,22 @@
         container.appendChild(gridline)
       }
     }
-    renderGridlines('x')
-    draggableGridlines('x')
 
-    renderGridlines('y')
-    draggableGridlines('y')
-  })();
+    // External API.
+    exports.renderGridlines = function () {
+      // Remove existing gridlines, in case this gets called multiple times.
+      clear()
 
-  // Image loader (form) for the prototype.
-  (function () {
+      renderGridlines('x')
+      draggableGridlines('x')
+
+      renderGridlines('y')
+      draggableGridlines('y')
+    }
+  })(gridiron)
+
+  // int main(void), which for this is managed by the form in the page.
+  const main = function () {
     // Development/debug
     document.querySelector('#gridiron-image-loader-src').value = 'http://placekitten.com/800/600'
 
@@ -91,17 +142,17 @@
 
       const src = this.querySelector('#gridiron-image-loader-src').value
       if (src) {
-        imagePreloader(src, function (error, preloaded) {
+        gridiron.image.load(src, function (error, preloaded) {
           if (error) {
-            // Shouldn't actually get here.
+            // rethrow for now
             throw error
           }
-          const img = document.querySelector('#gridiron-map-image')
-          img.src = src
-          img.style.maxHeight = preloaded.height + 'px'
-          img.style.maxWidth = preloaded.width + 'px'
+          // (re)render gridlines
+          gridiron.renderGridlines()
         })
       }
     }, false)
-  })()
+  }
+
+  main()
 })()
