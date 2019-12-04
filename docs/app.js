da62d093b6bb55fdc172961a34f4a19d881e5133
Jeremy Osborne <jeremywosborne@gmail.com>
2019-12-04 09:29:14 -0800

    chores

--- docs/app.js
+++ docs/app.js
@@ -1,13 +1,39 @@
 /* global interact */
+
+// Notes to future devs:
+//
+// - Choice of a non-transpiled, non-promise, non-import, callback style, older
+//   code base is intentional due to where this sample code will get ported to.
+//   Going old skool baby.
+// - Callback (presumed async) based functions should attempt to adhere to an
+//   arity of 2, with a signature of `f(data: object, callback: function (error, data))`.
+//   Need more args? Wrap in an object (acting as associative array).
 (function () {
   // Gridiron internals as old fashioned invoke immediate function.
   const gridiron = {};
 
+  //
+  // Async helper methods.
+  //
   (function (exports) {
-    // Async helper methods.
     exports.async = {}
 
-    const waterfall = function (funcs) {
+    //
+    // Chain callback style methods together.
+    //
+    // Taks `funcs` an array of functions, treated as a queue of workers, and
+    // returns a function to begin the process.
+    //
+    // First call passes any args to first function in the queue, all additional
+    // functions are called with the return value of the previous.
+    //
+    // If any error is encountered, the queue stops, and the error is passed
+    // as `callbacks.error(error)`. `callbacks.error` is not included, error
+    // is thrown. If included, `callbacks.done(data)` is
+    // called with the final return value from the final function in the queue.
+    //
+    const waterfall = function (funcs, callbacks) {
+      callbacks = callbacks || {}
       const next = function () {
         const f = funcs.shift()
         if (f) {
@@ -17,13 +43,19 @@
           args.push(function (error, data) {
             // ...if error, then throw...
             if (error) {
-              throw error
+              if (typeof callbacks.error === 'function') {
+                callbacks.error(error)
+              } else {
+                throw error
+              }
             }
             // ...otherwise next call until task queue is empty.
             next(data)
           })
           // Call the task with the arguments.
           f.apply(null, args)
+        } else if (typeof callbacks.done === 'function') {
+          callbacks.done.apply(null, Array.prototype.slice.call(arguments))
         }
       }
 
@@ -44,15 +76,18 @@
     // This will make an HTTP request to the server and fetch the existing
     // data, either an image URI and no coordinate data (if no grid has been created)
     // or an image URI and coordinate data.
+    // `src: string` is URI to read from, and assumed to be all we'll need.
     // Callback is invoked with `callback(error, data)`
-    const read = function (callback) {
-      // Faux async
+    const read = function (src, callback) {
+      //
+      // Faux http request.
+      //
       setTimeout(function () {
         // WARNING: Data structure not finalized, and this is faux data (in case
         // the placekitten reference didn't set off your warning signals).
         callback(null, {
-          mapURI: 'http://placekitten.com/800/600',
-          coords: []
+          mapURI: src,
+          coords: [],
         })
       }, 600)
     }
@@ -148,9 +183,6 @@
 
   // Grid controllers for the prototype.
   (function (exports) {
-    // Pixels between gridlines...
-    const MIN_DEFAULT_DISTANCE = 100
-
     // Clears any existing gridlines.
     const clear = function () {
       const nodes = document.querySelectorAll('[class*="gridiron-gridline"]')
@@ -186,23 +218,38 @@
       })
     }
 
-    // axis is `x` or `y`
-    const renderGridlines = function (axis) {
+    //
+    // `axis: string` is `x` or `y`
+    // `num: number` is integer value of how many gridlines to draw on the image
+    // if included, or if not included lets the app decide a default number of
+    // gridlines.
+    //
+    // TODO: Switch to default num? 6 gridlines?
+    //
+    const renderGridlines = function (axis, num) {
       const dims = gridiron.image.dimensions()
-      // We only need only enough gridlines to cover the image.
-      const offset = axis === 'x' ? dims.width : dims.height
+      const imageWidth = axis === 'x' ? dims.width : dims.height
       // Length of gridline cuts across entire container.
       const gridlineLength = axis === 'x' ? dims.container.height : dims.container.width
-      // Container here is the container of the gridlines, not the map container.
+      // Calculate number of gridlines and the space between them.
+      let distanceBetweenGridlines = 100
+      if (!num) {
+        // Assume default distance, calculate default number of gridlines.
+        num = Math.floor(imageWidth / distanceBetweenGridlines)
+      } else {
+        // Fit all of the requested gridlines in as best as possible.
+        distanceBetweenGridlines = Math.floor(imageWidth / num)
+      }
+
+      // Gridline container that will hold our gridlines.
       const container = document.querySelector('#gridiron-map-gridlines-' + axis)
-      const num = Math.floor(offset / MIN_DEFAULT_DISTANCE)
       // Grid contains start and end boundaries.
       for (let i = 0; i < num + 1; i++) {
         const gridline = document.createElement('div')
         gridline.className = 'gridiron-gridline-' + axis
         // Dynamically set height since gridline is child of a positioned container.
         gridline.style[axis === 'x' ? 'height' : 'width'] = gridlineLength + 'px'
-        gridline.style[axis === 'x' ? 'left' : 'top'] = (dims.offset[axis] + i * MIN_DEFAULT_DISTANCE) + 'px'
+        gridline.style[axis === 'x' ? 'left' : 'top'] = (dims.offset[axis] + i * distanceBetweenGridlines) + 'px'
         container.appendChild(gridline)
       }
     }
@@ -224,21 +271,26 @@
   const main = function () {
     const taskQueue = gridiron.async.waterfall([
       gridiron.crud.read,
+      // Unwrap the data we need and pass down the pipeline.
       function (data, callback) {
-        const src = data.mapURI
-        if (src) {
-          // Setup the page for grid editing.
-          gridiron.image.load(src, function (error) {
-            // Call passed in callback when done.
-            callback(error)
-          })
-        }
+        // if we don't have this, it's a problem.
+        callback(null, data.mapURI)
       },
+      gridiron.image.load,
       gridiron.renderGridlines,
-    ])
+    ], {
+      // Let it throw for now. If errors become a problem, which they will
+      // with HTTP, we should notify with a problem.
+      // error: function (error) {console.log('you haz error:', error)},
+      //
+      // We don't need done, since the final function in the queue doesn't
+      // return anything we need.
+      // done: function (data) {console.log('you haz data:', data)},
+    })
     // Originally had the idea of passing in some data, but not needed at the
     // moment. Kick off the task queue.
-    taskQueue()
+    // DEMO ONLY SRC.
+    taskQueue('http://placekitten.com/800/600')
   }
 
   main()
